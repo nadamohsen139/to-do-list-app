79fe5f4210efc3acd2d5176cdfc50f6e4d83834c
nadamohsen139 <mohsennada139@gmail.com>
2024-05-14 15:17:25 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const inputBtn = document.querySelector("button");
+inputBtn.addEventListener("click", function addTask() {
+    if(inputBox.value === "") {
+        alert("You Did not write a task!");
+    }
+    else {
+        let li = document.createElement("LI");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("SPAN");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+});
+listContainer.addEventListener("click", (e) => {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+};
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
